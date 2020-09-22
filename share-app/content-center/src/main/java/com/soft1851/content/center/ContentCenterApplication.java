ce41a03e6ab54e9077dd265a82b83e28fe3c5e24
flygit <1224178565@qq.com>
2020-09-22 15:17:29 +0800

    整合alibaba-cloud

---
+++ share-app/content-center/src/main/java/com/soft1851/content/center/ContentCenterApplication.java
@@ -0,0 +1,16 @@
+package com.soft1851.content.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xunmi
+ */
+@SpringBootApplication
+public class ContentCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentCenterApplication.class, args);
+    }
+
+}
